c10f3228e36716862d84eb8a365f3b448f1a25b7
Hasyidan Paramananda <hasyidanparamananda@gmail.com>
2018-10-18 13:32:52 +0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,61 @@
+chrome.runtime.onInstalled.addListener(function(){
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
+			  pageUrl: {hostContains: 'facebook.com'},
+			})
+			],
+		    actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
+	});
+});
+
+var BgSet = '';
+var FbSet = '';
+var transparent = 'if(document.getElementById("contentCol")!=null)document.getElementById("contentCol").style.backgroundColor = "transparent"; if(document.getElementById("pagelet_timeline_recent")!=null)document.getElementById("pagelet_timeline_recent").style.backgroundColor = "transparent";'
+var setting = 'document.body.style.backgroundSize = "100% auto"; document.body.style.backgroundPosition = "center top"; document.body.style.backgroundRepeat = "repeat-y";'; //default
+
+chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
+	if(changeInfo["status"] == "complete" && (tab["url"]!="" || tab["url"]!=null)){
+		// console.log("Update");
+		// console.log(tabID);
+		// console.log(changeInfo);
+		// console.log(tab);
+	 	if(localStorage["BackgroundColor"] != null || localStorage["BackgroundColor"] != ''){
+	    	BgSet += 'document.body.style.backgroundColor = "' + localStorage["BackgroundColor"] + '"; ';
+	    }
+	 	if(localStorage["BackgroundImage"] != null || localStorage["BackgroundImage"] != ''){
+	    	BgSet += 'document.body.style.backgroundImage = "url(' + localStorage["BackgroundImage"] + ')"; ';
+	    }
+	    if(localStorage["BarColor"] != null || localStorage["BarColor"] != ''){
+	    	FbSet +=	'document.getElementById("bluebarRoot").firstChild.style.backgroundColor = "' + localStorage["BarColor"] + '";'+
+						'document.getElementById("bluebarRoot").firstChild.style.border = "1px solid ' + localStorage["BarColor"] + '";';
+	    }
+	    if(BgSet!="" || FbSet!=""){
+	    	if (localStorage["Setting"] == 'CenterNo') {
+		        var setting = 'document.body.style.backgroundSize = "100% auto"; document.body.style.backgroundPosition = "center"; document.body.style.backgroundRepeat = "no-repeat"; document.body.style.backgroundAttachment = "fixed";';
+		    }
+		    else if (localStorage["Setting"] == 'CenterWith') {
+		        var setting = 'document.body.style.backgroundSize = "100% auto"; document.body.style.backgroundPosition = "center top"; document.body.style.backgroundRepeat = "repeat-y"; document.body.style.backgroundAttachment = "";';
+		    }
+	    	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+				chrome.tabs.executeScript(
+					tabs[0].id,
+					{code:  
+						BgSet+
+						FbSet+
+						transparent+
+						setting
+					},
+					function(){
+						if (chrome.runtime.lastError){
+					        var errorMsg = chrome.runtime.lastError.message;
+					        if (errorMsg == "Cannot access a chrome:// URL"){
+					            /*Error Handling Here*/
+					        }
+					    }
+					})
+				});
+	    }
+	}
+});
---
+++ popup.js
@@ -0,0 +1,151 @@
+let colorBoxBackground = document.getElementById('colorBoxBackground');
+let imageBoxBackground = document.getElementById('imageBoxBackground');
+let blueBarBoxBackground = document.getElementById('blueBarBoxBackground');
+
+var colour;
+var setting;
+
+function truncate(n, len) {
+    var ext = n.substring(n.lastIndexOf(".") + 1, n.length).toLowerCase();
+    var filename = n.replace('.' + ext,'');
+    if(filename.length <= len) {
+        return n;
+    }
+    filename = filename.substr(0, len) + (n.length > len ? '[...]' : '');
+    return filename + '.' + ext;
+};
+
+colorBoxBackground.onchange = function(){
+	console.log("MasukColor");
+	localStorage["BackgroundColor"] = colorBoxBackground.value;
+	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 	'document.body.style.backgroundColor = "' + localStorage["BackgroundColor"] + '";'+
+					'if(document.getElementById("contentCol")!=null)document.getElementById("contentCol").style.backgroundColor = "transparent";'+
+					'if(document.getElementById("pagelet_timeline_recent")!=null)document.getElementById("pagelet_timeline_recent").style.backgroundColor = "transparent";'});
+	});
+}
+
+function resetColorBox(){
+	var reset;
+	if(localStorage["BackgroundImage"] == null || localStorage["BackgroundImage"] == ''){
+		reset = 'if(document.getElementById("contentCol")!=null)document.getElementById("contentCol").style.backgroundColor = "";'+
+				'if(document.getElementById("pagelet_timeline_recent")!=null)document.getElementById("pagelet_timeline_recent").style.backgroundColor = "";';
+	} else {
+		reset = '';
+	}
+	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 	'document.body.style.backgroundColor = "";'+
+					reset});
+	});
+	localStorage["BackgroundColor"] = '';
+}
+
+imageBoxBackground.onchange = function(){
+	console.log("MasukImage");
+	var fileName = $(this).val();
+	$(this).next('.custom-file-label').html(truncate(fileName.replace(/^.*[\\\/]/, ''), 15));
+	var tgt = this.target || window.event.srcElement, 
+	files = tgt.files;
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function () {
+            localStorage["BackgroundImage"] = fr.result;
+            //document.body.style.backgroundImage = 'url(' + fr.result + ')'; //Change Body background become image
+            chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+			chrome.tabs.executeScript(
+				tabs[0].id,
+				{code:  'document.body.style.backgroundImage = "url(' + fr.result + ')";'+ 
+						'if(document.getElementById("contentCol")!=null)document.getElementById("contentCol").style.backgroundColor = "transparent";'+
+						'if(document.getElementById("pagelet_timeline_recent")!=null)document.getElementById("pagelet_timeline_recent").style.backgroundColor = "transparent";'+
+						setting});
+			});
+        }
+        fr.readAsDataURL(files[0]);
+    }
+}
+
+function resetImageBox(){
+	var reset = 'if(document.getElementById("contentCol")!=null)document.getElementById("contentCol").style.backgroundColor = "";'+
+				'if(document.getElementById("pagelet_timeline_recent")!=null)document.getElementById("pagelet_timeline_recent").style.backgroundColor = "";';
+	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 	'document.body.style.backgroundImage = "";'+
+					reset});
+	});
+	localStorage["BackgroundImage"] = '';
+}
+
+$('input[type=radio][name=optradio]').change(function () {
+	if (this.value == 'CenterNo') {
+        localStorage["Setting"] = 'CenterNo';
+        var setting = 'document.body.style.backgroundSize = "100% auto"; document.body.style.backgroundPosition = "center"; document.body.style.backgroundRepeat = "no-repeat"; document.body.style.backgroundAttachment = "fixed";';
+    }
+    else if (this.value == 'CenterWith') {
+        localStorage["Setting"] = 'CenterWith';
+        var setting = 'document.body.style.backgroundSize = "100% auto"; document.body.style.backgroundPosition = "center top"; document.body.style.backgroundRepeat = "repeat-y"; document.body.style.backgroundAttachment = "";';
+    }
+    if(localStorage["BackgroundImage"] != null || localStorage["BackgroundImage"] != ''){
+    	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+			chrome.tabs.executeScript(
+				tabs[0].id,
+				{code: setting});
+		});
+    }
+});
+
+blueBarBoxBackground.onchange = function(){
+	console.log("MasukBlueBar");
+	localStorage["BarColor"] = blueBarBoxBackground.value;
+	
+	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 	'document.getElementById("bluebarRoot").firstChild.style.backgroundColor = "' + localStorage["BarColor"] + '";'+
+					'document.getElementById("bluebarRoot").firstChild.style.border = "1px solid ' + localStorage["BarColor"] + '";'
+					});
+	});
+}
+
+function resetBlueBarBox(){
+	var reset = 'document.getElementById("bluebarRoot").firstChild.style.backgroundColor = "";'+
+				'document.getElementById("bluebarRoot").firstChild.style.border = "";';
+	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 	reset});
+	});
+	localStorage["BarColor"] = '';
+}
+
+
+
+function resetClearAll(){
+	var reset = 'document.body.style.backgroundColor = "";'+
+				'document.body.style.backgroundImage = "";'+
+				'document.getElementById("bluebarRoot").firstChild.style.backgroundColor = "";'+
+				'document.getElementById("bluebarRoot").firstChild.style.border = "";'+
+				'if(document.getElementById("contentCol")!=null)document.getElementById("contentCol").style.backgroundColor = "";'+
+				'if(document.getElementById("pagelet_timeline_recent")!=null)document.getElementById("pagelet_timeline_recent").style.backgroundColor = "";';
+	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 	reset});
+	});
+	localStorage.clear();
+}
+
+$( document ).ready(function() {
+	$('#colorBoxBackground').val(localStorage["BackgroundColor"]);
+	$('#colorBoxBackground').colorpicker();
+	$('#blueBarBoxBackground').val(localStorage["BarColor"]);
+	$('#blueBarBoxBackground').colorpicker();
+	$("#ResetColor").click(resetColorBox);
+	$("#ResetImage").click(resetImageBox);
+	$("#ResetFbBar").click(resetBlueBarBox);
+	$("input[name=optradio][value=" + localStorage["Setting"] + "]").prop('checked', true);
+});
