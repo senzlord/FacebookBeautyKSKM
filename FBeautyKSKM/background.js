b3f681d96e044c7f92fbe1999d1ee9d25e24d444
Hasyidan Paramananda <hasyidanparamananda@gmail.com>
2018-10-23 17:05:28 +0700

    Update

--- FBeautyKSKM/background.js
+++ FBeautyKSKM/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function(){
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [new chrome.declarativeContent.PageStateMatcher({
-			  pageUrl: {hostContains: 'facebook.com'},
+			  pageUrl: {hostContains: 'www.facebook.com'},
 			})
 			],
 		    actions: [new chrome.declarativeContent.ShowPageAction()]
@@ -10,17 +10,59 @@ chrome.runtime.onInstalled.addListener(function(){
 	});
 });
 
-var BgSet = '';
-var FbSet = '';
-var transparent = 'if(document.getElementById("content")!=null)document.getElementById("content").firstChild.firstChild.style.backgroundColor = "transparent";if(document.getElementById("contentCol")!=null)document.getElementById("contentCol").style.backgroundColor = "transparent"; if(document.getElementById("pagelet_timeline_recent")!=null)document.getElementById("pagelet_timeline_recent").style.backgroundColor = "transparent";'
-var setting = 'document.body.style.backgroundSize = "100% auto"; document.body.style.backgroundPosition = "center top"; document.body.style.backgroundRepeat = "repeat-y";'; //default
 
 chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
-	if(changeInfo["status"] == "complete" && (tab["url"]!="" || tab["url"]!=null)){
-		// console.log("Update");
-		// console.log(tabID);
-		// console.log(changeInfo);
-		// console.log(tab);
+	console.log("Update");
+	console.log(tabID);
+	console.log(changeInfo);
+	console.log(tab);
+	var BgSet = '';
+	var FbSet = '';
+	var aSet = '';
+	var transparent = 'if(document.getElementById("content")!=null){document.getElementById("content").firstChild.firstChild.style.backgroundColor = "transparent";} if(document.getElementById("contentCol")!=null){document.getElementById("contentCol").style.backgroundColor = "transparent";} if(document.getElementById("pagelet_timeline_recent")!=null){document.getElementById("pagelet_timeline_recent").style.backgroundColor = "transparent";}'
+	var setting = 'document.body.style.backgroundSize = "100% auto"; document.body.style.backgroundPosition = "center top"; document.body.style.backgroundRepeat = "repeat-y";'; //default
+	var copyFB = 
+		'if(document.getElementById("fbTimelineHeadline")!=null){'+
+			'var Div = document.getElementById("fbTimelineHeadline").childNodes[1].firstChild;'+
+			// 'var getClass = Div.firstChild.getElementsByTagName("a")[0].getAttribute("class");'+
+			// 'var classes = getClass.split(" ");'+
+			'var Listing = document.createElement("li");'+
+			'var element = document.createElement("a");'+
+			'element.setAttribute("class", "_6-6");'+
+			'element.setAttribute("id", "FBCopyLink");'+
+			'element.innerHTML = "Copy";'+
+			'element.addEventListener("click", function() {'+
+		    	'var link = "www.facebook.com/profile.php?id="+localStorage["ProfileID"];'+
+		    	'var dummy = document.createElement("input");'+
+				'document.body.appendChild(dummy);'+
+				'dummy.setAttribute("id", "dummy_id");'+
+				'document.getElementById("dummy_id").value=link;'+
+				'dummy.select();'+
+				'document.execCommand("copy");'+
+				'alert("Permanent Link Copied");'+
+			'});'+
+			'Listing.appendChild(element);'+
+			'if(document.getElementById("FBCopyLink")==null){'+
+				'Div.appendChild(Listing);'+
+			'}'+
+		'}';
+	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code:  
+				copyFB
+			},
+			function(){
+				if (chrome.runtime.lastError){
+			        var errorMsg = chrome.runtime.lastError.message;
+			        if (errorMsg == "Cannot access a chrome:// URL"){
+			            /*Error Handling Here*/
+			        }
+			    }
+			})
+		});
+	if((tab["url"]!="" || tab["url"]!=null)){
+	//if(changeInfo["status"] == "complete" && (tab["url"]!="" || tab["url"]!=null)){
 	 	if(localStorage["BackgroundColor"] != null || localStorage["BackgroundColor"] != ''){
 	    	BgSet += 'document.body.style.backgroundColor = "' + localStorage["BackgroundColor"] + '"; ';
 	    }
@@ -32,7 +74,7 @@ chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
 	    				'document.getElementById("bluebarRoot").firstChild.style.backgroundColor = "' + localStorage["BarColor"] + '";'+
 						'document.getElementById("bluebarRoot").firstChild.style.border = "1px solid ' + localStorage["BarColor"] + '";}';
 	    }
-	    if(BgSet !="" || FbSet !=""){
+	    if(BgSet !="" || FbSet !="" || aSet != ""){
 	    	if (localStorage["Setting"] == 'CenterNo') {
 		        var setting = 'document.body.style.backgroundSize = "100% auto"; document.body.style.backgroundPosition = "center"; document.body.style.backgroundRepeat = "no-repeat"; document.body.style.backgroundAttachment = "fixed";';
 		    }
@@ -45,6 +87,7 @@ chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
 					{code:  
 						BgSet+
 						FbSet+
+						aSet+
 						transparent+
 						setting
 					},
@@ -60,3 +103,5 @@ chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
 	    }
 	}
 });
+
+
--- FBeautyKSKM/content.js
+++ FBeautyKSKM/content.js
@@ -1,5 +1,5 @@
 if(document.getElementById("pagelet_timeline_main_column")!=null){
-	var Div = document.getElementById("pagelet_timeline_main_column")
+	var Div = document.getElementById("pagelet_timeline_main_column");
 	var JSONData = Div.getAttribute("data-gt");
 	JSONData = JSON.parse(JSONData);
 	if (JSONData!=null) {
@@ -9,7 +9,33 @@ if(document.getElementById("pagelet_timeline_main_column")!=null){
 			localStorage["FBName"] = document.getElementById("fb-timeline-cover-name").innerText;
 		}
 		var line = document.createElement("span");
-		line.innerHTML = localStorage["ProfileID"];
+		line.setAttribute('class','alternate_name profileID');
+		line.innerHTML = " "+localStorage["ProfileID"];
 		document.getElementById("fb-timeline-cover-name").firstChild.appendChild(line);
+	}
+}
+
+if(document.getElementById("fbTimelineHeadline")!=null){
+	var Div = document.getElementById("fbTimelineHeadline").childNodes[1].firstChild;
+	// var getClass = Div.firstChild.getElementsByTagName("a")[0].getAttribute("class");
+	// var classes = getClass.split(" ");
+	var Listing = document.createElement("li");
+	var element = document.createElement("a");
+	element.setAttribute('class', "_6-6");
+	element.setAttribute('id', "FBCopyLink");
+	element.innerHTML = "Copy";
+	element.addEventListener("click", function() {
+    	var link = "www.facebook.com/profile.php?id="+localStorage["ProfileID"];
+    	var dummy = document.createElement("input");
+		document.body.appendChild(dummy);
+		dummy.setAttribute("id", "dummy_id");
+		document.getElementById("dummy_id").value=link;
+		dummy.select();
+		document.execCommand("copy");
+		alert("Permanent Link Copied");
+	});
+	Listing.appendChild(element);
+	if(document.getElementById("FBCopyLink")==null){
+		Div.appendChild(Listing);
 	}
 }
